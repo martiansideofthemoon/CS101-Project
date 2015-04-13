69330ca10f209d84d19c591a847f58ed62690aa6
Karan Chadha <karanchadhaiitb@gmail.com>
2015-04-13 17:41:48 +0530

    display working

--- graphics.cpp
+++ graphics.cpp
@@ -130,8 +130,23 @@ int main(int argc, char **argv){
          break;
       }
       else if (ev.type==ALLEGRO_EVENT_MOUSE_BUTTON_UP){
-               arrow_counter++;
-               rectangle_x1+=60;
+                if(arrow_counter==solved_moves_counter-1)
+                  {
+                  arrow_counter=0;
+                  rectangle_x1=45;
+                  rectangle_x2=105;
+                  startpoint_x=start_x_origin + endpoints[solved_moves[arrow_counter]][0]*0.5*scale_coordinate_to_pixel;
+                  startpoint_y=start_y_origin + endpoints[solved_moves[arrow_counter]][1]*sqrt(3)/2*scale_coordinate_to_pixel;
+                  endpoint_x  =start_x_origin + endpoints[solved_moves[arrow_counter]][2]*0.5*scale_coordinate_to_pixel;
+                  endpoint_y  =start_y_origin + endpoints[solved_moves[arrow_counter]][3]*sqrt(3)/2*scale_coordinate_to_pixel;
+                  if(solved_moves[arrow_counter]==6||solved_moves[arrow_counter]==7||solved_moves[arrow_counter]==14||solved_moves[arrow_counter]==15)
+                  isback=true;
+               else isback=false;
+                  }
+                  else 
+                {
+                  arrow_counter++;
+                rectangle_x1+=60;
                rectangle_x2+=60;
                startpoint_x=start_x_origin + endpoints[solved_moves[arrow_counter]][0]*0.5*scale_coordinate_to_pixel;
                startpoint_y=start_y_origin + endpoints[solved_moves[arrow_counter]][1]*sqrt(3)/2*scale_coordinate_to_pixel;
@@ -140,19 +155,9 @@ int main(int argc, char **argv){
                if(solved_moves[arrow_counter]==6||solved_moves[arrow_counter]==7||solved_moves[arrow_counter]==14||solved_moves[arrow_counter]==15)
                   isback=true;
                else isback=false;
-               if(arrow_counter==solved_moves_counter)
-               {
-                  arrow_counter=0;
-                  rectangle_x1=600;
-                  rectangle_x2=662.5;
-                  startpoint_x=start_x_origin + endpoints[solved_moves[arrow_counter]][0]*0.5*scale_coordinate_to_pixel;
-                  startpoint_y=start_y_origin + endpoints[solved_moves[arrow_counter]][1]*sqrt(3)/2*scale_coordinate_to_pixel;
-                  endpoint_x  =start_x_origin + endpoints[solved_moves[arrow_counter]][2]*0.5*scale_coordinate_to_pixel;
-                  endpoint_y  =start_y_origin + endpoints[solved_moves[arrow_counter]][3]*sqrt(3)/2*scale_coordinate_to_pixel;
-
+           
                }
-               }
-
+            }
       if(redraw && al_is_event_queue_empty(event_queue)) {
          redraw = false;
          x_origin=start_x_origin;
@@ -246,28 +251,28 @@ void draw_arrow(int start_point_x ,int start_point_y ,int end_point_x,int end_po
    b2=end_point_y;
    length=sqrt((a1-a2)*(a1-a2)+(b1-b2)*(b1-b2));
    if(!isBack){  
-   al_draw_line(start_point_x, start_point_y, end_point_x, end_point_y, al_map_rgb(255,0,0), 2.5);
+   al_draw_line(start_point_x, start_point_y, end_point_x, end_point_y, al_map_rgb(255,0,0), 5);
    x1=  a2;
    y1=  b2;
    x2=  x1+((a1-a2)+(b1-b2))/length*sqrt(2)*20;
    y2=  y1+(-(a1-a2)+(b1-b2))/length*sqrt(2)*20;
    x3=  x1+((a1-a2)-(b1-b2))/length*sqrt(2)*20;
    y3=  y1+((a1-a2)+(b1-b2))/length*sqrt(2)*20;
-   al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(255,255,0));
+   al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(255,0,0));
    }
    else {
       al_draw_arc((start_point_x+end_point_x)/2,
-                  (start_point_y+end_point_y)/2,
+                  (start_point_y+end_point_y)/2-100,
                   fabs((start_point_y-end_point_y)/2),
                   PI/2,fabs((start_point_y-end_point_y)/2)/((start_point_y-end_point_y)/2)*PI ,
-                  al_map_rgb(255,0,0), 2.5);
+                  al_map_rgb(255,0,0), 5);
                x1=a1+fabs((start_point_y-end_point_y)/2)/((start_point_y-end_point_y)/2)*25;
-               y1=min(b2,b1);
+               y1=min(b2,b1)-100;
                x2=a2;
                y2=y1-25;
                x3=a2;
                y3=y1+25;
-      al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(255,255,0));
+      al_draw_filled_triangle(x1, y1, x2, y2, x3, y3, al_map_rgb(255,0,0));
 
    }
 
